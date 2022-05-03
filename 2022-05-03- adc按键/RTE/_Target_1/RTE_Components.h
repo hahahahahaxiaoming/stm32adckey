7e446029b73b737e5b4ca6c1c07195f177e175b1
hahahahahaxiaoming <104843679+hahahahahaxiaoming@users.noreply.github.com>
2022-05-04 00:29:33 +0800

    Add files via upload

---
+++ 2022-05-03- adc按键/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,25 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Template' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/* Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/* Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
+#define RTE_DEVICE_STDPERIPH_RCC
+
+
+#endif /* RTE_COMPONENTS_H */
